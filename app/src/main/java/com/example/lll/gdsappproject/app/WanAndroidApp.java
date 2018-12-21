ee04a0fac44b2c39ecbd1bd3c6c023201eea3151
lizhe998 <125956902@qq.com>
2018-12-22 00:24:53 +0800

    初始化

---
+++ app/src/main/java/com/example/lll/gdsappproject/app/WanAndroidApp.java
@@ -0,0 +1,26 @@
+package com.example.lll.gdsappproject.app;
+
+import android.app.Activity;
+import android.app.Application;
+
+import com.squareup.leakcanary.RefWatcher;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasActivityInjector;
+
+public class WanAndroidApp extends Application  implements HasActivityInjector {
+
+
+    @Inject
+    DispatchingAndroidInjector<Activity> dispatchingAndroidInjector;
+
+    private static WanAndroidApp instance;
+    private RefWatcher refWatcher;
+    @Override
+    public AndroidInjector<Activity> activityInjector() {
+        return dispatchingAndroidInjector;
+    }
+}

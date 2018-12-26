2a673696eebb35540aa4908890df7765cbf0155c
lizhe998 <125956902@qq.com>
2018-12-26 23:41:17 +0800

    初始化

--- app/src/main/java/com/example/lll/gdsappproject/base/activity/AbstractSimpleActivity.java
+++ app/src/main/java/com/example/lll/gdsappproject/base/activity/AbstractSimpleActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.SupportActivity;
 
+import com.example.lll.gdsappproject.component.ActivityCollector;
+
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
@@ -15,9 +17,43 @@ public abstract class AbstractSimpleActivity extends SupportActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutId());
-        unbinder =ButterKnife.bind(this);
+        unbinder = ButterKnife.bind(this);
         mActivity = this;
+        ActivityCollector.getInstance().addActivity(this);
+        onViewCreated();
+        initToolbar();
+        initEventAndData();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.getInstance().removeActivity(this);
+        if (unbinder != null && unbinder != Unbinder.EMPTY) {
+            unbinder.unbind();
+            unbinder = null;
+        }
+    }
+
+    /**
+     * 在initEventAndData()之前执行
+     */
+    protected abstract void onViewCreated();
+
+    /**
+     * 获取当前Activity的UI布局
+     *
+     * @return 布局id
+     */
     protected abstract int getLayoutId();
+
+    /**
+     * 初始化ToolBar
+     */
+    protected abstract void initToolbar();
+
+    /**
+     * 初始化数据
+     */
+    protected abstract void initEventAndData();
 }
---
+++ app/src/main/java/com/example/lll/gdsappproject/base/activity/BaseActivity.java
@@ -0,0 +1,63 @@
+package com.example.lll.gdsappproject.base.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatDelegate;
+
+import com.example.lll.gdsappproject.base.presenter.AbstractPresenter;
+import com.example.lll.gdsappproject.base.view.AbstractView;
+
+import javax.inject.Inject;
+
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.support.HasSupportFragmentInjector;
+
+public abstract class BaseActivity<T extends AbstractPresenter> extends AbstractSimpleActivity
+        implements HasSupportFragmentInjector, AbstractView {
+
+    @Inject
+    DispatchingAndroidInjector<Fragment> mFragmentDispatchingAndroidInjector;
+    @Inject
+    protected T mPresenter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onViewCreated() {
+        if (mPresenter != null) {
+            mPresenter.attachView(this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        if (mPresenter != null) {
+            mPresenter.detachView();
+            mPresenter = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public AndroidInjector<Fragment> supportFragmentInjector() {
+        return  mFragmentDispatchingAndroidInjector;
+    }
+
+    @Override
+    public void useNightMode(boolean isNightMode) {
+        if (isNightMode){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+        recreate();
+    }
+}
